c6e42037bc0c622eee6f15941aba177c07e2b24d
nouri1234x <143615068+nouri1234x@users.noreply.github.com>
2023-09-03 13:06:50 +0200

    Add files via upload

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -3,7 +3,10 @@ if [ "$#" -ne 2 ] ; then
  echo "number of arguments shall be 2" 
  exit 1
 fi
-
+if ! [[ "$1" =~ ^\\/$|^((\\/([a-zA-Z0-9_-]+))+)$ ]]; then
+ echo "file cannot be created"
+ exit 1
+fi
 if !   mkdir -p "${1%/*}"  ; then 
  echo "file cannot be created"
  exit 1
@@ -12,11 +15,12 @@ if ! touch "$1" ; then
 echo "file cannot be created"
 exit 1
 fi
-if ! echo "$2" > "$1" ; then
-echo "file cannot be created"
-exit 1
-fi
 if [ -f "$1" ] ; then
 echo "file cannot be created"
 exit 1
 fi
+if ! echo "$2" > "$1" ; then
+echo "file cannot be created"
+exit 1
+fi
+
